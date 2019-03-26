5ec2fb99e5081c8faeee6d9f2f93d3ecff683df5
Satiata <info@satiata.nl>
2019-03-26 22:33:27 +0100

    Initial commit

---
+++ src/Controller/ChoresController.php
@@ -0,0 +1,32 @@
+<?php
+
+    namespace App\Controller;
+
+    class ChoresController
+    {
+
+        public function ListChores() {
+
+
+
+        }
+
+        public function AddChores() {
+
+
+
+        }
+
+        public function RemoveChores() {
+
+
+
+        }
+
+        public function UpdateChores() {
+
+
+
+        }
+
+    }
